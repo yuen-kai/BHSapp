f772f0d30d6b0a818e420909e1deef25257b25f9
ihavenoexperience <processedcheese@outlook.com>
2024-11-28 10:39:10 -0500

    bhs website

--- app/(tabs)/bhswebsite.tsx
+++ app/(tabs)/bhswebsite.tsx
@@ -4,15 +4,12 @@ import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function HomeScreen() {
   return (
-    <WebView source={{ uri: 'https://bhs.brookline.k12.ma.us/' }} style={styles.WebViewStyle}></WebView>
+    <SafeAreaView style={{flex: 1}}>
+      <WebView source={{ uri: 'https://bhs.brookline.k12.ma.us/' }}></WebView>
+    </SafeAreaView>
   )
 }
-
-const styles = StyleSheet.create({
-  WebViewStyle: {
-    flex: 1,
-  }
-});
