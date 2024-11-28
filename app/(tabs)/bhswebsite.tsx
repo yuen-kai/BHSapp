e53620160b0161469e523d4af007b12a45badcb6
ihavenoexperience <processedcheese@outlook.com>
2024-11-28 10:39:43 -0500

    bhs website

--- app/(tabs)/bhswebsite.tsx
+++ app/(tabs)/bhswebsite.tsx
@@ -1,9 +1,4 @@
-import { Image, StyleSheet, Platform } from 'react-native';
 import {WebView} from 'react-native-webview'
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function HomeScreen() {
