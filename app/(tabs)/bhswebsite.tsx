fbc290685a6574dda97e8fe6f7868f8092972570
ihavenoexperience <processedcheese@outlook.com>
2024-11-28 10:19:17 -0500

    add bhs website

---
+++ app/(tabs)/bhswebsite.tsx
@@ -0,0 +1,18 @@
+import { Image, StyleSheet, Platform } from 'react-native';
+import {WebView} from 'react-native-webview'
+import { HelloWave } from '@/components/HelloWave';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+
+export default function HomeScreen() {
+  return (
+    <WebView source={{ uri: 'https://bhs.brookline.k12.ma.us/' }} style={styles.WebViewStyle}></WebView>
+  )
+}
+
+const styles = StyleSheet.create({
+  WebViewStyle: {
+    flex: 1
+  }
+});
