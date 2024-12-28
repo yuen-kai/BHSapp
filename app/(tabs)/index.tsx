645587fc34e9d8f2e3fee5a008cf898f7b7d4ddb
ihavenoexperience <processedcheese@outlook.com>
2024-12-28 11:11:38 -0500

    progress on index ui althiough scaling is looking pretty bad

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -13,14 +13,41 @@ export default function HomeScreen() {
     rmNum: number
   }
   let courses:Course[] = [{name:"English",teacher:"teacherName", block: "A", rmNum: 324}, {name:"Math",teacher:"teacherName2", block: "B", rmNum: 124}]
-  const TimeLeftText = ({timeLeft}: any) => {
-    return <Text style={{fontSize: 18, zIndex: 3}}>{timeLeft} Minutes Remaining</Text>
+  const TimeLeftText = ({timeLeft}: any) => {//position and top are placeholders
+    return <Text style={{fontSize: 18, zIndex: 3, position: 'absolute', top: 348, alignSelf: 'center'}}>{timeLeft} Minutes Remaining</Text>
+  }
+  const CourseDataText = ({ valueName = 'N/A', val = 'N/A' }: { valueName?: string; val?: string }) => {
+    let leftPos = '15%'
+    let topPos = '60%'
+    if (valueName == 'Block' || valueName == 'Room') {
+      leftPos = '70%'
+    }
+    if (valueName == 'Teacher' || valueName == 'Room') {
+      topPos = '75%'
+    }
+    return <Text style={{fontSize: 18, left: leftPos, position: 'absolute', top: topPos, textAlign: 'center'}}>{valueName}{'\n'}{val}</Text>
   }
   return (
-    <SafeAreaView style={{paddingTop: 10, flex: 1, width: '100%', height: '100%'}}>
-      <ProgressBar progress={0.5} color={MD3Colors.error50} style={{position: 'absolute', flex: 1, height: 50, borderRadius: 30, alignSelf: 'center', top: 300, width: '75%'}}>
-        <TimeLeftText timeLeft={0}></TimeLeftText>
-      </ProgressBar>
+    <SafeAreaView style={{paddingTop: 10, flex: 1, width: '100%', height: '100%', backgroundColor: 'blue'}}>
+      <View style={styles.headerContainer}>
+
+      </View>
+      <ProgressBar progress={0.5} color={MD3Colors.error50} style={{position: 'absolute', height: 50, borderRadius: 30, alignSelf: 'center', width: '75%', marginTop: '20%'}}></ProgressBar>
+      <TimeLeftText timeLeft={0}></TimeLeftText>
+      
+      <CourseDataText valueName={'Block'} val={'B'}></CourseDataText>
+      <CourseDataText valueName={'Teacher'} val={'placeholder'}></CourseDataText>
+      <CourseDataText valueName={'Room'} val={'200'}></CourseDataText>
     </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    backgroundColor: 'yellow',
+    top: '5%',
+    width: '75%',
+    alignSelf: 'center',
+    height: '31%'
+  }
+})
