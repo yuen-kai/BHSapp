b80770d23d8f09ef7696a862af6e53e498345c47
yuen-kai <cykai168@gmail.com>
2025-01-30 10:28:51 -0500

    Course type changes

--- app/(tabs)/addcourses.tsx
+++ app/(tabs)/addcourses.tsx
@@ -16,7 +16,8 @@ type Course = {
 	name: string;
 	teacher: string;
 	block: string;
-	term?: string;
+	lunch: number; // 1 = first lunch, 2 = 2nd lunch
+	term: number;  // 0 = full year, 1 = first semester, 2 = 2nd semester
 	roomNumber: string;
 };
 
