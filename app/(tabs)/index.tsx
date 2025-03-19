cb3d4b7f74d476edf85d14160dc8cd4c226d9c68
yuen-kai <cykai168@gmail.com>
2025-03-19 11:21:56 -0400

    fix rounding warning

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -159,7 +159,7 @@ export default function HomeScreen() {
 			if (nearestStart && nearestEnd) {//still sometimes loss in precision in certain minutes, sometimes 55.00000001 or smth
 				const totalMinutes = getDifferenceInMinutes(nearestStart, nearestEnd);
 				const minutesPast = getDifferenceInMinutes(nearestStart, new Date());
-				const progressValue = parseFloat(clamp((minutesPast) / totalMinutes, 0, 1).toFixed(2));
+				const progressValue = clamp(Math.round(minutesPast / totalMinutes * 100)/100, 0, 1);
 				setProgress(progressValue);
 			}
 		}, 1000);
